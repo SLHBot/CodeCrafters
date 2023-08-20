56254c74c0a0c72fd37aa9807d7ca95942bef87f
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2023-08-20 23:52:20 +0530

    1

--- src/getConfig.js
+++ src/getConfig.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports.getConfig = () => {
     return {
-        name: process.env.NAME || 'Code Crafters',
+        name: process.env.NAME || 'CodeCrafters',
         prefix: process.env.PREFIX || '@',
         writesonicAPI: process.env.WRITE_SONIC || null,
         bgAPI: process.env.BG_API_KEY || null,
